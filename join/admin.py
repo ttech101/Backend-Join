04e5277e1577ecc9c69043c19cb37abda73f4dff
Tony <ttech@gmx.de>
2023-12-21 08:31:47 +0100

    update

--- join/admin.py
+++ join/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact,Task
 
 # Register your models here.
 
-# class ContactAdmin(admin.ModelAdmin):
-#     list_display = ('name','email','author','receiver')
-#     search_fields = ('name',)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','email','author','receiver')
+    search_fields = ('name',)
 
 
-# class TaskAdmin(admin.ModelAdmin):
-#     list_display = ('headline','date','author','receiver')
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('headline','date','author','receiver')
 
-# admin.site.register(Task, TaskAdmin)
+admin.site.register(Task, TaskAdmin)
 
-# admin.site.register(Contact, ContactAdmin)
+admin.site.register(Contact, ContactAdmin)
