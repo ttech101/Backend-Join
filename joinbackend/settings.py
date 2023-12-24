b6ca620aa3993b6742e0e248cb1472a672cba2e7
Tony <ttech@gmx.de>
2023-12-24 13:40:20 +0100

    update

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lq_5_n$2n94l6hpikgj60o#*_aadkhrfj(d3dvp)4g$xk)zvug
 DEBUG = False
 
 ALLOWED_HOSTS = ['join-django.tech-mail.eu','ttech101.pythonanywhere.com']
-CORS_ALLOWED_ORIGINS = ['join-django.tech-mail.eu','ttech101.pythonanywhere.com']
+CORS_ALLOWED_ORIGINS = ['join-django.tech-mail.eu','ttech101.pythonanywhere.com','https://join-django.tech-mail.eu',]
 # CORS_ALLOW_ALL_ORIGINS = True
 
 
