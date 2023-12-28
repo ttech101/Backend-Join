879eaa5df7d4e141eb6ba2a36b255b77d1c3ec0a
Tony <ttech@gmx.de>
2023-12-28 10:31:58 +0100

    update

--- join/tests.py
+++ join/tests.py
@@ -8,45 +8,45 @@ from .models import Contact, Task
 
 class YourAppTestCase(TestCase):
     def setUp(self):
-        # Erstellen Sie einen Testbenutzer
+        # Create a test user
         self.user = User.objects.create_user(
             username='testuser',
             password='testpassword',
             email='test@example.com'
         )
-        # Erstellen Sie einen Token für den Testbenutzer
+        # Create a token for the test user
         self.token = Token.objects.create(user=self.user)
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_login_view(self):
-        # Überprüfen Sie, ob der Anmeldungs-Endpunkt funktioniert
+        # Check if the login endpoint is functioning
         response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'testpassword'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Fügen Sie weitere Überprüfungen hinzu, basierend auf Ihren Anforderungen
-        self.assertIn('token', response.data)  # Überprüfen Sie, ob der Token im Antwortdaten vorhanden ist
+        # Add more checks based on your requirements
+        self.assertIn('token', response.data)  # Check if the token is present in the response data
 
     def test_contact_creation(self):
-        # Überprüfen Sie, ob Sie einen Kontakt erstellen können
+        # Check if you can create a contact
         data = {'author': 'testuser', 'receiver': 'testuser', 'name': 'Test Contact', 'email': 'test@example.com', 'hex_color': '#FFFFFF', 'logogram': 'ABC'}
         response = self.client.post(reverse('contact'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Fügen Sie weitere Überprüfungen hinzu, basierend auf Ihren Anforderungen
+        # Add more checks based on your requirements
         contact = Contact.objects.get(name='Test Contact')
-        self.assertEqual(contact.author.username, 'testuser')  # Überprüfen Sie den Benutzernamen des Autors
+        self.assertEqual(contact.author.username, 'testuser')  # Check the author's username
 
     def test_task_creation(self):
-        # Überprüfen Sie, ob Sie eine Aufgabe erstellen können
+        # Check if you can create a task
         data = {'headline': 'Test Task', 'text': 'Test task description', 'priority': 'High'}
         response = self.client.post(reverse('task'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Fügen Sie weitere Überprüfungen hinzu, basierend auf Ihren Anforderungen
+        # Add more checks based on your requirements
         task = Task.objects.get(headline='Test Task')
-        self.assertEqual(task.text, 'Test task description')  # Beispiel für eine zusätzliche Überprüfung
+        self.assertEqual(task.text, 'Test task description')  # Example of an additional check
 
-    # Fügen Sie weitere Tests für andere Funktionen hinzu
+    # Add more tests for other functions
 
     def tearDown(self):
-        # Räumen Sie auf, indem Sie erstellte Objekte löschen oder andere notwendige Schritte unternehmen
-        Contact.objects.all().delete()  # Beispiel: Löschen Sie alle erstellten Kontakte
-        Task.objects.all().delete()  # Beispiel: Löschen Sie alle erstellten Aufgaben
+        # Clean up by deleting created objects or taking other necessary steps
+        Contact.objects.all().delete()  # Example: Delete all created contacts
+        Task.objects.all().delete()  # Example: Delete all created tasks
